2cc244cd2e50efc4ea1cbe55735abfb4eac302aa
heliopn <hpaivan@gmail.com>
2020-06-05 05:55:59 -0300

    PROCFILE

--- api/routes/BDayRoutes.js
+++ api/routes/BDayRoutes.js
@@ -3,12 +3,12 @@ module.exports = function(app) {
   var bdayList = require('../controllers/BDayController');
 
   // todoList Routes
-  app.route('/name')
+  app.route('/')
     .get(bdayList.show_All_names)
     .post(bdayList.create_a_name);
 
 
-  app.route('/name/:url')
+  app.route('/:url')
     .get(bdayList.read_a_url)
     .put(bdayList.update_a_url)
     .delete(bdayList.delete_a_url);
